2b29cd0601dc6e4e65b339d737375dd786d69bfa
Christian Medeiros <Christianmedeiros2014@hotmail.com>
2024-02-26 21:56:27 -0300

    Criando a entidade comanda

--- gestao_comanda_api/src/main/java/br/edu/infnet/gestaocomanda/model/Comanda.java
+++ gestao_comanda_api/src/main/java/br/edu/infnet/gestaocomanda/model/Comanda.java
@@ -3,12 +3,20 @@ package br.edu.infnet.gestaocomanda.model;
 import java.util.Date;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+
+@Entity(name="tabelacomanda")
 @EqualsAndHashCode
 @ToString
 @NoArgsConstructor
@@ -16,26 +24,36 @@ public class Comanda {
 	
 	@Getter
 	@Setter
+	@Column(name="id_comanda")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Getter
 	@Setter
+	@Column
+	@Transient
 	private Cliente cliente;
 	
 	@Getter
 	@Setter
+	@Column(name="nr_mesa")
 	private int numeroMesa;
 	
 	@Getter
 	@Setter
+	@Column(name="dt_abertura")
 	private Date dataHoraAbertura;
 	
 	@Getter
 	@Setter
+	@Column(name="dt_fechamento")
 	private Date dataHoraFechamento;
 	
 	@Getter
 	@Setter
+	@Column(name="pedidos")
+	@Transient
 	private List<Pedido> pedidos;
 
 }
