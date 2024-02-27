36b0b93e3f056bfeffe5f09a542171d86ec4da3b
Christian Medeiros <Christianmedeiros2014@hotmail.com>
2024-02-26 21:09:54 -0300

    Projeto Inicial

---
+++ gestao_comanda_api/src/main/java/br/edu/infnet/gestaocomanda/GestaoComandaApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.gestaocomanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoComandaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoComandaApiApplication.class, args);
+	}
+
+}
